c412d417736b8c3272c490505cf76f28ffb04d60
Irwin <irwinbravooporto@gmail.com>
2023-04-14 14:43:56 -0500

    Initial Commit

---
+++ Pagos/Pagos.h
@@ -0,0 +1,18 @@
+//
+//  Pagos.h
+//  Pagos
+//
+//  Created by Irwin Bravo Oporto on 14/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pagos.
+FOUNDATION_EXPORT double PagosVersionNumber;
+
+//! Project version string for Pagos.
+FOUNDATION_EXPORT const unsigned char PagosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pagos/PublicHeader.h>
+
+
