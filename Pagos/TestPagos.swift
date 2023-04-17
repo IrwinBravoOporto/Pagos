e434806834fb6a04aec63a5c13b26d5acd70f9ae
Irwin <irwinbravooporto@gmail.com>
2023-04-17 18:01:25 -0500

    Test

---
+++ Pagos/TestPagos.swift
@@ -0,0 +1,22 @@
+//
+//  TestPagos.swift
+//  Pagos
+//
+//  Created by Irwin Bravo Oporto on 17/04/23.
+//
+
+import Foundation
+import CryptoSwift
+import Alamofire
+
+
+public class EmailValidator {
+
+    public static func isValid(email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPredicate.evaluate(with: email)
+    }
+
+}
+
